2a8915df399d3de6ebc26f5d6128be2e4b06a0c2
Huntertk <factsofuniverse8@gmail.com>
2022-12-31 23:43:00 +0530

    First Version

--- index.js
+++ index.js
@@ -2,28 +2,20 @@ const characters = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", 
     "/"];
 let buttonEvent = document.getElementById("btn")
 let generatedPassword = document.getElementById("password")
+let generatedPassword1 = document.getElementById("password1")
+
+function genPass() {
+    return Math.floor(Math.random() * characters.length)
+}
 
 buttonEvent.addEventListener("click", function () {
+    generatedPassword.textContent = characters[genPass()] + characters[genPass()] + characters[genPass()] + characters[genPass()] + characters[genPass()] + characters[genPass()] + characters[genPass()] + characters[genPass()] +characters[genPass()] +
+        characters[genPass()] + characters[genPass()] +
+        characters[genPass()] + characters[genPass()] + characters[genPass()] + characters[genPass()]
 
-    let randomNumber1 = Math.floor(Math.random() * 91)
-    let randomNumber2 = Math.floor(Math.random() * 91)
-    let randomNumber3 = Math.floor(Math.random() * 91)
-    let randomNumber4 = Math.floor(Math.random() * 91)
-    let randomNumber5 = Math.floor(Math.random() * 91)
-    let randomNumber6 = Math.floor(Math.random() * 91)
-    let randomNumber7 = Math.floor(Math.random() * 91)
-    let randomNumber8 = Math.floor(Math.random() * 91)
-    let randomNumber9 = Math.floor(Math.random() * 91)
-    let randomNumber10 = Math.floor(Math.random() * 91)
-    let randomNumber11 = Math.floor(Math.random() * 91)
-    let randomNumber12 = Math.floor(Math.random() * 91)
-    let randomNumber13 = Math.floor(Math.random() * 91)
-    let randomNumber14 = Math.floor(Math.random() * 91)
-    let randomNumber15 = Math.floor(Math.random() * 91)
-
-    generatedPassword.textContent = characters[randomNumber1] + characters[randomNumber2] + characters[randomNumber3] + characters[randomNumber4] + characters[randomNumber5] + characters[randomNumber6] + characters[randomNumber7] + characters[randomNumber8] + characters[randomNumber9] +
-        characters[randomNumber10] + characters[randomNumber11] +
-        characters[randomNumber12] + characters[randomNumber13] + characters[randomNumber14] + characters[randomNumber15]
+        generatedPassword1.textContent = characters[genPass()] + characters[genPass()] + characters[genPass()] + characters[genPass()] + characters[genPass()] + characters[genPass()] + characters[genPass()] + characters[genPass()] +characters[genPass()] +
+        characters[genPass()] + characters[genPass()] +
+        characters[genPass()] + characters[genPass()] + characters[genPass()] + characters[genPass()]
 
 })
 
